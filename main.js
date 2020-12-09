682b7117d572d65acdd659e7eb703bf7c2b745ba
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-12-09 22:27:28 +0900

    Activate the menu in that section when scrolling

--- main.js
+++ main.js
@@ -92,7 +92,68 @@ const contactRights = document.querySelector("#contact .contact__rights");
 const year = new Date().getFullYear();
 contactRights.innerHTML = `&copy ${year} Like the brave LionKing - All rights reserved`;
 
+// 1. Bring all section elements and menu items
+// 2. Observe all sections using IntersectionObserver
+// 3. Activate the menu item corresponding to the section shown
+const sectionIds = [
+  "#home",
+  "#about",
+  "#skills",
+  "#portfolio",
+  "#goodWords",
+  "#contact",
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove("active");
+  selectedNavItem = selected;
+  selectedNavItem.classList.add("active");
+}
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // Scrolling goes down and the page goes up
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener("wheel", () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
